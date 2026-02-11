1ff24dec3a83fc0c005e6e547f9b73cea92a7110
Jamal Khan <jamaljamali11220@gmail.com>
2026-02-11 21:29:33 +0500

    fixed some bugs in expect() toBe()

--- js/email.test.js
+++ js/email.test.js
@@ -4,12 +4,12 @@ describe('isValidEmail', () => {
     test('return false if Email is not valid', () =>{
         const email = "user123@gmail.net";
         const result = isValidEmail(email);
-        expect(isValidEmail(email)).toBe('false')
+        expect(result).toBe(false)
     });
 
     test('return true if Email is valid', () => {
         const email = "user123@gmail.com";
         const result = isValidEmail(email);
-        expect(isValidEmail(email)).toBe('true')
+        expect(result).toBe(true)
     })
 })
